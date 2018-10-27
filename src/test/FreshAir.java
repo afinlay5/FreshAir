bf2f10fcec06b00febca986af844300d17529788
Adrian D. Finlay <adf5152@live.com>
2018-10-27 02:40:47 -0400

    spacing addressed

--- src/test/FreshAir.java
+++ src/test/FreshAir.java
@@ -33,11 +33,15 @@ import javafx.scene.Scene;
 import javafx.geometry.Pos;
 import javafx.geometry.Insets;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Font;
+import javafx.scene.control.Button;
 // import javafx.scene.paint.Paint;
 import javafx.stage.StageStyle;
+import javafx.scene.layout.Priority;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
@@ -55,11 +59,13 @@ public class FreshAir extends Application {
 	private static int height = 620, width =1024;
 	private static Background bkg;
 	private static Image app;
-	private static Image middleImg = new Image ("bkg.png", FreshAir.width,FreshAir.height*.9, false, true, true );
+	private static Image middleImg = new Image ("bkg.png", FreshAir.width,FreshAir.height*.95, false, true, true );
+	private static ImageView home, back, divider, fwd;
+	private static Button bHome = new Button(), bBack = new Button(), bFwd = new Button();
 
 
 	/* Launch JavaFX application */
-	public static void home (String[] args) { FreshAir.launch(args); }
+	public static void main (String[] args) { FreshAir.launch(args); }
 
 	/*  Connect to the API first.  */
 	private static final String AirVisual_AQI_API = "https://http://api.airvisual.com/v2/city?city=Los%20Angeles&state=California&country=USA&key=fQryFDkwpvMtiZ8A7";
@@ -75,68 +81,90 @@ public class FreshAir extends Application {
 	};
 
     @Override // JavaFX Lifecycle Method #2
-    public void start(Stage home) {
+    public void start(Stage main) {
 
-		/* Home Screen */
+		/* main Screen */
 		
         //Give the stage a title.
-		home.setTitle("FreshAir® - Clean Air Map!");
+		main.setTitle("FreshAir® - Clean Air Map!");
 
 
 		// Provide Icon
 		app = new Image(getClass().getResource("app.png").toExternalForm());
-		home.getIcons().add(app);
+		main.getIcons().add(app);
 
 		// Provide Icon
-		// home.getIcons().add(app);
+		// main.getIcons().add(app);
 
 		//Root Node
 		VBox rootNode = new VBox();
 
 		/*Components*/
 
-		//TopRow
-		HBox topRow = new HBox();
-		Label lbl = new Label("Fresh Air");
-		lbl.setFont(Font.font("Simplex", 25.14));
-		topRow.getChildren().add(lbl);
-		topRow.setAlignment(Pos.CENTER);
-		// topRow.setBackground(	new Background(	new BackgroundFill(Paint.valueOf("#9cdef6"), CornerRadii.EMPTY, Insets.EMPTY ) ) );
-		topRow.setBackground(	new Background(	new BackgroundFill(Color.rgb(156,206, 246, 0.35), CornerRadii.EMPTY, Insets.EMPTY ) ) ); 
-		topRow.setMinHeight(FreshAir.height*.06);
-		topRow.setPrefHeight(FreshAir.height*.07);
-		topRow.setMaxHeight(FreshAir.height*.10);
+		//upperRow		
+		HBox upperRow = new HBox();
+		upperRow.setMinHeight(FreshAir.height*.94);
+		upperRow.setPrefHeight(FreshAir.height*.94);
+		upperRow.setMaxHeight(FreshAir.height*.94);
+		upperRow.setBackground(new Background (new BackgroundImage (FreshAir.middleImg, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT)) );
 
+		//upperInnerRow
+		HBox upperInnerRow = new HBox();
+		upperInnerRow.setAlignment(Pos.CENTER);
+		upperInnerRow.setBackground(  new Background(	new BackgroundFill(Color.rgb(230, 234, 244, 0.35), CornerRadii.EMPTY, Insets.EMPTY ) ) );
 
-		//MiddleRow
-		ImageView middleImgV = new ImageView(FreshAir.middleImg);
-		HBox middleRow = new HBox(middleImgV);
+		TextField search = new TextField (); 
+		search.setPromptText("Specify a City, Province, Country . . .");
+		search.setPrefColumnCount(20);
+		search.setAlignment(Pos.CENTER);	
+
+		upperInnerRow.getChildren().add(search);
+
+		//upperInnerRow -> upperRow
+		upperRow.getChildren().add(upperInnerRow);
+
+		//set alignment
+		upperRow.setAlignment(Pos.CENTER);
+
 
 
 		//BottomRow
-		HBox bottomRow = new HBox();
-		bottomRow.setMinHeight(FreshAir.height*.01);
-		bottomRow.setPrefHeight(FreshAir.height*.03);
-		bottomRow.setMaxHeight(FreshAir.height*.04);
-		bottomRow.setBackground( new Background (new BackgroundFill(Color.GREY, CornerRadii.EMPTY, Insets.EMPTY ) ) ); 
+		BorderPane bottomRow = new BorderPane();
+		bottomRow.setPadding(new Insets(03, 10, 02, 10));
+		bottomRow.setMinHeight(FreshAir.height*.06);
+		bottomRow.setPrefHeight(FreshAir.height*.06);
+		bottomRow.setMaxHeight(FreshAir.height*.06);
+		bottomRow.setBackground( new Background (new BackgroundFill(Color.LIGHTGREY, CornerRadii.EMPTY, Insets.EMPTY ) ) ); 
+
+		home = new ImageView ( new Image(getClass().getResource("home.png").toExternalForm(),20,20,true,true)); bHome.setGraphic(home);
+		back = new ImageView ( new Image(getClass().getResource("bck.png").toExternalForm()) ); bBack.setGraphic(back);
+		// divider = new Image(getClass().getResource("div.png").toExternalForm());
+		fwd = new ImageView ( new Image(getClass().getResource("fwd.png").toExternalForm()) ); bFwd.setGraphic(fwd);
+		
+		Label lbl = new Label("Breathe in, Breathe fresh . . . ®");
+		lbl.setFont(Font.font("Simplex", javafx.scene.text.FontWeight.LIGHT, javafx.scene.text.FontPosture.ITALIC, 10));
+
+		bottomRow.setLeft(bHome);
+		bottomRow.setCenter(lbl);
+		bottomRow.setRight( new HBox(bBack, bFwd)) ;
 
 
 		//Components -> SceneGraph
-		rootNode.getChildren().addAll(topRow, middleRow, bottomRow);
+		rootNode.getChildren().addAll(upperRow, bottomRow);
 
         //Scene
         Scene scene = new Scene(rootNode, FreshAir.width, FreshAir.height); 
 
 
-   		//Restrict resizeable
-		home.setResizable(false);
+   		// Restrict resizeable
+		// main.setResizable(false);
 
 		//Make Transparent
-		home.initStyle(StageStyle.UNDECORATED);
+		main.initStyle(StageStyle.UNDECORATED);
 
         //Set Scene, Show stage
-        home.setScene(scene);
-        home.show();
+        main.setScene(scene);
+        main.show();
 
 	};
 
